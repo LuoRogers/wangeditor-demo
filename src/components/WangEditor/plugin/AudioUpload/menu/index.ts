f0a85ba17fcb68a168d0d81e2576b80447de3fb6
LuoRogers <LuoRogers@outlook.com>
2025-04-23 11:08:22 +0800

    feat: 新增音频上传配置

--- src/components/WangEditor/plugin/AudioUpload/menu/index.ts
+++ src/components/WangEditor/plugin/AudioUpload/menu/index.ts
@@ -4,12 +4,14 @@
  */
 
 import AudioMenu from '../audioMenu'
+import { genUploadAudioMenuConfig } from './config'
 
 export const uploadAudioMenuConf = {
   key: 'uploadAudio', // menu key ，唯一。注册之后，可配置到工具栏
   factory() {
     return new AudioMenu()
   },
+  cinfig:genUploadAudioMenuConfig()
 
   // 默认的菜单菜单配置，将存储在 editorConfig.MENU_CONF[key] 中
   // 创建编辑器时，可通过 editorConfig.MENU_CONF[key] = {...} 来修改
